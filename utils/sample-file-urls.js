c3fb7c9214feccff25bc21ea90f9e51fda359346
Abdul Basit <dev.basit2020@gmail.com>
2024-06-22 20:36:17 +0500

    Added sample file urls

---
+++ utils/sample-file-urls.js
@@ -0,0 +1,4 @@
+export const SampleFileUrls = {
+  BASE_LOGO_URL: "https://storage.googleapis.com/bestone_media/assets/logo.svg",
+  FAV_ICON_URL: "https://storage.googleapis.com/bestone_media/assets/fav-icon.svg",
+};
