0f36923f0f25d8e24842c677f1d758a505723a2e
Abdul Basit <dev.basit2020@gmail.com>
2024-06-23 11:54:26 +0500

    Added common endpoints file

---
+++ utils/endpoints/common.js
@@ -0,0 +1,8 @@
+export const CommonEndpoints = {
+  BaseUrl: process.env.APP_BASE_URL,
+  OAuth: "/v1/api/oauth/token",
+  Logout: "/v1/api/logout",
+  Authenicate: "/v1/api/authenticate",
+  Upload: "/v1/api/upload/",
+  SystemSettings: "/v1/api/MAN/configuration",
+};
