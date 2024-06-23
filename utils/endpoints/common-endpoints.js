ca34da9070509ac58a9a149aed435959cb87ec72
Abdul Basit <dev.basit2020@gmail.com>
2024-06-23 15:41:53 +0500

    Renamed

--- utils/endpoints/common-endpoints.js
+++ utils/endpoints/common-endpoints.js
@@ -1,8 +1,7 @@
 export const CommonEndpoints = {
-  BaseUrl: process.env.APP_BASE_URL,
-  OAuth: "/v1/api/oauth/token",
   Logout: "/v1/api/logout",
   Authenicate: "/v1/api/authenticate",
   Upload: "/v1/api/upload/",
-  SystemSettings: "/v1/api/MAN/configuration",
+  TenantSettings: "/v1/api/MAN/configuration",
+  Settings: "/v1/api/MAN/user/update/setting",
 };
