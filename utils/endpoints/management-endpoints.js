35553ac8bf701b3b1355c38cc30d196552f23f93
Abdul Basit <dev.basit2020@gmail.com>
2024-06-24 10:53:56 +0500

    Added management endpoints

---
+++ utils/endpoints/management-endpoints.js
@@ -0,0 +1,13 @@
+export const ManagementEndpoints = {
+  Users: "/v1/api/MAN/user/get/employ/list",
+  Clients: "/v1/api/MAN/client/get/list",
+  Drivers: "/v1/api/MAN/driver/get/list",
+  Countries: "/v1/api/MAN/country/get/list",
+  Cities: "/v1/api/MAN/city/get/all/list",
+  Pickers: "/v1/api/MAN/user/get/picker/list",
+  Partners: "/v1/api/MAN/partners/get/all",
+  Tiers: "/v1/api/MAN/tier/get/all",
+  Warehouses: "/v1/api/MAN/warehouse/get/list",
+};
+
+export const ManagementClientsEndpoints = {};
