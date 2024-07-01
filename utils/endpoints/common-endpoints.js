bfeb9fe3b0c7ad323fd81bc54c57edb65d0f72f8
Abdul Basit <dev.basit2020@gmail.com>
2024-07-01 08:20:41 +0500

    Added token endpoint

--- utils/endpoints/common-endpoints.js
+++ utils/endpoints/common-endpoints.js
@@ -1,6 +1,7 @@
 export const CommonEndpoints = {
-  Logout: "/v1/api/logout",
+  OAuth: "/v1/api/oauth/token",
   Authenicate: "/v1/api/authenticate",
+  Logout: "/v1/api/logout",
   Upload: "/v1/api/upload/",
   TenantSettings: "/v1/api/MAN/configuration",
   Settings: "/v1/api/MAN/user/update/setting",
