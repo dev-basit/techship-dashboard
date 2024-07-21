4c0113f1383463064d56a9e4bd41d310721c4faa
Abdul Basit <dev.basit2020@gmail.com>
2024-07-21 05:21:04 +0500

    Added new icon & import changed for some icons

--- utils/icons.js
+++ utils/icons.js
@@ -1,18 +1,20 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
-import ArrowDown from "@/assets/images/arow_down.svg";
+import ArrowDown from "@/assets/images/arow-down.svg";
+import ArrowUp from "@/assets/images/arrow-up.svg";
 import Close from "@/assets/images/close.svg";
 import Error from "@/assets/images/failure.svg";
 import Menu from "@/assets/images/hamburger.svg";
-import ProfileDummy from "@/assets/images/dummy_profile.png";
+import ProfileDummy from "@/assets/images/dummy-profile.png";
 import Success from "@/assets/images/success.svg";
-import LeftArrow from "@/assets/images/leftArrow.svg";
-import RightArrow from "@/assets/images/rightArrow.svg";
-import HoverLeftArrow from "@/assets/images/hoverLeftArrow.svg";
-import HoverRighttArrow from "@/assets/images/hoverRightArrow.svg";
+import LeftArrow from "@/assets/images/left-arrow.svg";
+import RightArrow from "@/assets/images/right-arrow.svg";
+import HoverLeftArrow from "@/assets/images/hover-left-arrow.svg";
+import HoverRighttArrow from "@/assets/images/hover-right-arrow.svg";
 import GoToDetailsPage from "@/assets/images/detail.svg";
 
 export const Icons = {
+  ArrowUp,
   ArrowDown,
   Close,
   Error,
